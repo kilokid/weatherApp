0f260317723ee7e28beac8f609dc3f3367d9e6b3
kilokid <arlomov007@gmail.com>
2022-01-04 22:31:34 +0300

    feat: getting the user's coordinate and displaying the weather at its location

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -10,43 +10,30 @@ class App extends Component {
   }
 
   state = {
-    temp: [],
+    temp: null,
   }
 
-  // weatherService = new WeatherService();
+  weatherService = new WeatherService();
 
-  // onUpdateTemp = (temp) => {
-  //   this.setState({ temp: temp });
-  // }
-
-  // getTemp = () => {
-  //   this.weatherService
-  //     .gettingWeather()
-  //     .then(this.onUpdateTemp);
-  // }
-
-  getRes = async () => {
-    const _apiKey = '312b3e4a5c0f7918f5b3787c71cdc370';
-
-    let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Petersburg&appid=${_apiKey}&lang=ru&units=metric`);
-
-    if (!res.ok) {
-        throw new Error(`Произошла ошибка при получении погоды, статус ошибки: ${res.status}`);
-    }
-
-    return await res.json();
+  onUpdateTemp = (temp) => {
+    this.setState({ temp });
   }
 
   getTemp = () => {
-    this.getRes()
-      .then(res => this.setState({temp: res.main.temp}));
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.weatherService.getResourse(position.coords.latitude, position.coords.longitude)
+        .then(res => {
+          this.onUpdateTemp(Math.round(res.main.temp));
+        });
+
+    });
   }
 
   render() {
     const {temp} = this.state;
 
     return (
-        <h2>Температура в Питере: {Math.round(temp)}</h2>
+        <h2>Температура в Питере: {temp} °C</h2>
     );
   }
 }
--- src/services/WeatherService.js
+++ src/services/WeatherService.js
@@ -1,24 +1,29 @@
 class WeatherService {
     #apiKey = '312b3e4a5c0f7918f5b3787c71cdc370';
 
-    getResourse = async (url) => {
-        let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=Petersburg&appid=${this.#apiKey}`);
+    // GetUserLocation = async () => {
+        // navigator.geolocation.getCurrentPosition((position) => {
+        //     this.getResourse(position.coords.latitude, position.coords.longitude)
+        //         .then(res => {
+        //             const temp = this.#transformWeather(res);
+        //             return temp;
+        //         });
+        // });
+    // }
+        
+    getResourse = async (lat, lon) => {
+        let res = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${this.#apiKey}&units=metric`);
 
         if (!res.ok) {
-            throw new Error(`Произошла ошибка при получении погоды, статус ошибки: ${res.status}`);
+            throw new Error(`Произошла ошибка при получении погоды, статус ошибки: ${res.status}`)
         }
 
         return await res.json();
     }
 
-    gettingWeather = async () => {
-        const res = await this.getResourse();
-        return this.#transformFormCardWeather(res);
-    }
-
-    #transformFormCardWeather = (weather) => {
+    #transformWeather = (weather) => {
         return {
-            temp: weather.main.temp,
+            temp: Math.ceil(weather.main.temp),
         };
     }
 }
