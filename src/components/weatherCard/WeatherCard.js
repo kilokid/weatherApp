84cb1aa0449b90e65c7d5a139d0e5b71cf3fddea
kilokid <arlomov007@gmail.com>
2022-01-11 00:33:56 +0300

    fix: reduced the code and state of the weather component

--- src/components/weatherCard/WeatherCard.js
+++ src/components/weatherCard/WeatherCard.js
@@ -6,37 +6,20 @@ import SwitchImg from '../../services/SwitchImg';
 import './weatherCard.scss';
 
 const WeatherCard = () => {
-    const [temp, setTemp] = useState(null);
-    const [place, setPlace] = useState(null);
-    const [icon, setIcon] = useState(null);
-    const [feelsLike, setFeelsLike] = useState(null);
-    const [description, setDescription] = useState(null);
+    const [currentGeo, setCurrentGeo] = useState({});
 
-    const {getTempByCoords} = useWeatherService(); 
+    const {getTempByCoords} = useWeatherService();
 
     useEffect(() => {
         onRequest();
     }, []);
 
-    const onUpdateTemp = (temp, place, icon, feelsLike, description) => {
-        setTemp(temp);
-        setPlace(place);
-        setIcon(icon);
-        setFeelsLike(feelsLike);
-        setDescription(description)
-    }
-
     const onRequest = () => {
         navigator.geolocation.getCurrentPosition((position) => {
         getTempByCoords(position.coords.latitude, position.coords.longitude)
             .then(res => {
-                onUpdateTemp(
-                  res.temp, 
-                  res.place, 
-                  res.icon, 
-                  res.feelsLike,
-                  res.description,
-                );
+                const {temp, place, icon, feelsLike, description} = res;
+                setCurrentGeo({temp, place, icon, feelsLike, description});
             });
         });
     }
@@ -47,18 +30,18 @@ const WeatherCard = () => {
         return str[0].toUpperCase() + str.slice(1);
     }
 
-    let img = SwitchImg(icon);
+    let img = SwitchImg(currentGeo.icon);
 
     return (
         <div className="weather__card">
-            <h1 className='weather__city'>{place}</h1>
+            <h1 className='weather__city'>{currentGeo.place}</h1>
             <div className="weather__info">
                 <div className="weather__temp-info">
                     <img className='weather__icon' src={img} alt="" />
-                    <p className="weather__temp">{temp}°C</p>
+                    <p className="weather__temp">{currentGeo.temp}°C</p>
                 </div>
-                <p className="weather__description">{newDescription(description)}</p>
-                <p className="weather__feels">Ощущается как: {feelsLike}°C</p>
+                <p className="weather__description">{newDescription(currentGeo.description)}</p>
+                <p className="weather__feels">Ощущается как: {currentGeo.feelsLike}°C</p>
             </div>
         </div>
     );
