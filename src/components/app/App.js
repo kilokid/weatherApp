0b3f9201b972fa54d106b8b29e4e015f1e743936
kilokid <arlomov007@gmail.com>
2022-04-24 00:43:16 +0300

    feat: the main page logic has been moved and a router has been added

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,50 +1,23 @@
-import { useState, useEffect } from 'react';
-import useWeatherService from '../../services/WeatherService';
+import { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-
-import WeatherCard from '../weatherCard/WeatherCard';
-import InputSearch from '../inputSearch/InputSearch';
 
 import '../../styles/base.scss';
 import './app.scss';
 
+import Spinner from '../spinner/Spinner';
+
+const MainPage = lazy(() => import('../pages/MainPage'));
+
 const App = () => {
-  const [currentGeo, setCurrentGeo] = useState({});
-  const [coords, setCoords] = useState(false);
-
-  useEffect(() => {
-    updateWeather();
-  }, []);
-
-  const { getTempByCoords, getTempByCityName, error, loading } = useWeatherService();
-
-  const onWeatherRequest = (weather) => {
-    setCurrentGeo(weather);
-  };
-
-  const errorGetCoords = () => {
-    setCoords(true);
-  };
-
-  const updateWeather = () => {
-    navigator.geolocation.getCurrentPosition((position) => {
-      setCoords(false);
-
-      getTempByCoords(position.coords.latitude, position.coords.longitude).then(onWeatherRequest);
-    }, errorGetCoords);
-  };
-
-  const getWeatherForCity = (name) => {
-    getTempByCityName(name).then(onWeatherRequest);
-    setCoords(false);
-  };
-
   return (
     <Router>
-      <div className="container">
-        <InputSearch onAdd={getWeatherForCity} />
-        <WeatherCard currentGeo={currentGeo} error={error} loading={loading} coords={coords} />
-      </div>
+      <main>
+        <Suspense fallback={<Spinner />}>
+          <Routes>
+            <Route path="/weatherApp" element={<MainPage />} />
+          </Routes>
+        </Suspense>
+      </main>
     </Router>
   );
 };
---
+++ src/components/pages/MainPage.js
@@ -0,0 +1,47 @@
+import { useState, useEffect } from 'react';
+import useWeatherService from '../../services/WeatherService';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import WeatherCard from '../weatherCard/WeatherCard';
+import InputSearch from '../inputSearch/InputSearch';
+
+const MainPage = () => {
+  const [currentGeo, setCurrentGeo] = useState({});
+  const [coords, setCoords] = useState(false);
+
+  useEffect(() => {
+    updateWeather();
+  }, []);
+
+  const { getTempByCoords, getTempByCityName, error, loading } = useWeatherService();
+
+  const onWeatherRequest = (weather) => {
+    setCurrentGeo(weather);
+  };
+
+  const errorGetCoords = () => {
+    setCoords(true);
+  };
+
+  const updateWeather = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      setCoords(false);
+
+      getTempByCoords(position.coords.latitude, position.coords.longitude).then(onWeatherRequest);
+    }, errorGetCoords);
+  };
+
+  const getWeatherForCity = (name) => {
+    getTempByCityName(name).then(onWeatherRequest);
+    setCoords(false);
+  };
+
+  return (
+    <div className="container">
+      <InputSearch onAdd={getWeatherForCity} />
+      <WeatherCard currentGeo={currentGeo} error={error} loading={loading} coords={coords} />
+    </div>
+  );
+};
+
+export default MainPage;
